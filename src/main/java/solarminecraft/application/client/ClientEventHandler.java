6b201cba866055b0f8dbd21277112aac5fe39c5b
Shahrom <mshahromali@gmail.com>
2024-06-28 00:24:14 -0400

    Fix spacing

--- src/main/java/solarminecraft/application/client/ClientEventHandler.java
+++ src/main/java/solarminecraft/application/client/ClientEventHandler.java
@@ -20,24 +20,24 @@ public class ClientEventHandler {
 	private static int num = 0;
 	@SubscribeEvent
 	public void onRenderTick(RenderGuiOverlayEvent.Post event) {
-			if (mc.player != null && mc.level != null && !mc.options.hideGui && (mc.screen == null || (ConfigHandler.CLIENT.displayWithChatOpen.get() && mc.screen instanceof ChatScreen))) {
-				final Player player = mc.player;
-				float powerConsumption = ClientSetup.serverData.getlPower();
+		if (mc.player != null && mc.level != null && !mc.options.hideGui && (mc.screen == null || (ConfigHandler.CLIENT.displayWithChatOpen.get() && mc.screen instanceof ChatScreen))) {
+			final Player player = mc.player;
+			float powerConsumption = ClientSetup.serverData.getlPower();
 
-				String powerString = "Burning " + powerConsumption  + " Watts";
-				String solarStats = "Solar: " + ClientSetup.serverData.getPvVoltage() + "V | " + ClientSetup.serverData.getPvCurrent() + "A";
-				String timeRemainingString = "Time Remaining: " + ClientSetup.serverData.getTimeRemaining() + " Hours (" + ClientSetup.serverData.getBattRemaining() + "%)";
+			String powerString = "Burning " + powerConsumption  + " Watts";
+			String solarStats = "Solar: " + ClientSetup.serverData.getPvVoltage() + "V | " + ClientSetup.serverData.getPvCurrent() + "A";
+			String timeRemainingString = "Time Remaining: " + ClientSetup.serverData.getTimeRemaining() + " Hours (" + ClientSetup.serverData.getBattRemaining() + "%)";
 
-				int solarStatsColor = mc.level.isNight() ? 0xCAE34B : 0xCAE34B; // same for now.
+			int solarStatsColor = mc.level.isNight() ? 0xCAE34B : 0xCAE34B; // same for now.
 
-				// red if high, orange if mid, white if low
-				int powerConsumptionColor = powerConsumption > 20 ? 0xD6520B : powerConsumption <= 15 ? 0xFFFFFF : 0xCAE34B;
+			// red if high, orange if mid, white if low
+			int powerConsumptionColor = powerConsumption > 20 ? 0xD6520B : powerConsumption <= 15 ? 0xFFFFFF : 0xCAE34B;
 
 
-				RenderUtils.drawStringBottomLeft(event.getGuiGraphics(), solarStats,solarStatsColor, 1); // lowest line is 1.
-				RenderUtils.drawStringTopLeft(event.getGuiGraphics(), powerString, powerConsumptionColor, 0);
-				RenderUtils.drawStringCenter(event.getGuiGraphics(), timeRemainingString,0xFFFFFF, 0);
+			RenderUtils.drawStringBottomLeft(event.getGuiGraphics(), solarStats,solarStatsColor, 1); // lowest line is 1.
+			RenderUtils.drawStringTopLeft(event.getGuiGraphics(), powerString, powerConsumptionColor, 0);
+			RenderUtils.drawStringCenter(event.getGuiGraphics(), timeRemainingString,0xFFFFFF, 0);
 
-			}
+		}
 	}
 }
